590c70c81337846dbf18dac336f374c3ed03c540
EJ Jung <blesu@naver.com>
2017-02-22 18:02:11 +0900

    Initial commit

---
+++ src/main/ej/sample/mapper/TestMapper.java
@@ -0,0 +1,19 @@
+/**
+ * blesu
+ * 2017. 2. 21. 오후 4:02:32
+ */
+package ej.sample.mapper;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author blesu
+ *
+ */
+@Repository
+public interface TestMapper {
+	/**
+	 * test
+	 */
+	public void selectTest();
+}
