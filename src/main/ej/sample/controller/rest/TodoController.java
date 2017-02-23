904c474504eb387af02e79ab53ca93fed55c3b11
EJ Jung <blesu@naver.com>
2017-02-23 18:06:19 +0900

    todo.json 추가

--- src/main/ej/sample/controller/rest/TodoController.java
+++ src/main/ej/sample/controller/rest/TodoController.java
@@ -1,12 +1,16 @@
 package ej.sample.controller.rest;
 
-import lombok.Data;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.Data;
 
 /**
  * Created by blesu on 2017-02-23.
@@ -15,33 +19,40 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/rest")
 public class TodoController {
 
-    @RequestMapping(value = "/todo.json", method = RequestMethod.GET)
-    public ResponseEntity<ModelMap> execute(ModelMap model) throws Exception {
-        HttpStatus stat = HttpStatus.OK;
+	@RequestMapping(value = "/todo.json", method = RequestMethod.GET)
+	public ResponseEntity<ModelMap> execute(ModelMap model) throws Exception {
+		HttpStatus stat = HttpStatus.OK;
 
-        Item item = new Item();
-        item.setAction("Buy flower");
-        item.setDone(false);
-        model.addAttribute(item);
-        item = new Item();
-        item.setAction("Get Shoes");
-        item.setDone(false);
-        model.addAttribute(item);
-        item = new Item();
-        item.setAction("Collect Tickets");
-        item.setDone(true);
-        model.addAttribute(item);
-        item = new Item();
-        item.setAction("Call joe");
-        item.setDone(false);
-        model.addAttribute(item);
+		List<Item> itemList = new ArrayList<>();
 
-        return new ResponseEntity<>(model, stat);
-    }
+		Item item = new Item();
+		item.setAction("Buy flower");
+		item.setDone(false);
+		itemList.add(item);
 
-    @Data
-    private class Item {
-        private String action;
-        private boolean done;
-    }
+		item = new Item();
+		item.setAction("Get Shoes");
+		item.setDone(false);
+		itemList.add(item);
+
+		item = new Item();
+		item.setAction("Collect Tickets");
+		item.setDone(true);
+		itemList.add(item);
+
+		item = new Item();
+		item.setAction("Call joe");
+		item.setDone(false);
+		itemList.add(item);
+
+		model.addAttribute("items", itemList);
+
+		return new ResponseEntity<>(model, stat);
+	}
+
+	@Data
+	private class Item {
+		private String action;
+		private boolean done;
+	}
 }
